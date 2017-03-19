fac0a3985010f91868e13abf227368ca33ee119e
thassiov <tvmcarvalho@gmail.com>
2017-03-18 21:51:52 -0300

    Fazendo request para a API

--- rates.js
+++ rates.js
@@ -4,4 +4,25 @@
   * @license MIT
   */
 
+'use strict';
+
+const http = require('http');
+
+const API = {
+    hostname:'api.fixer.io',
+    path:'/latest?base=BRL'
+};
+
+http.get(API, function(res) {
+    let rates = '';
+
+    res.on('data', function(d) {
+        rates += d;
+    });
+
+    res.on('end', function() {
+        rates = JSON.parse(rates);
+        console.log(rates);
+    });
+});
 
